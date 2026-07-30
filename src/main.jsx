5f71b1e772ad53b7773adfaa70cb4e8a9b676f75
tiagojose1 <alexjosepais6@gmail.com>
2026-07-30 21:25:57 +0200

    fix: corrige caminho do AuthContext

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,10 @@ import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import "./index.css";
 
-
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
-
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-
     <AuthProvider>
-
       <App />
-
     </AuthProvider>
-
   </React.StrictMode>
-
 );
